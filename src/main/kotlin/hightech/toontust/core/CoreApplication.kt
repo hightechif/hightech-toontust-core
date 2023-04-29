7b90775dc131b7808c6131f68f8ddcc2bc1b1dfd
R. Fadhil <ridhan.fadhilah@sg-dsa.com>
2023-04-29 23:55:18 +0700

    First init

---
+++ src/main/kotlin/hightech/toontust/core/CoreApplication.kt
@@ -0,0 +1,11 @@
+package hightech.toontust.core
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CoreApplication
+
+fun main(args: Array<String>) {
+	runApplication<CoreApplication>(*args)
+}
