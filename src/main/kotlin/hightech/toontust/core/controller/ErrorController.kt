01092e7f67b3c0ec267527539332947942ac0615
R. Fadhil <ridhan.fadhilah@sg-dsa.com>
2023-05-18 02:49:53 +0700

    FEAT: API call script & Resonse Entity

--- src/main/kotlin/hightech/toontust/core/controller/ErrorController.kt
+++ src/main/kotlin/hightech/toontust/core/controller/ErrorController.kt
@@ -1,6 +1,8 @@
 package hightech.toontust.core.controller
 
 import hightech.toontust.core.dto.response.base.GeneralWrapper
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.RestControllerAdvice
 import javax.validation.ConstraintViolationException
@@ -9,13 +11,13 @@ import javax.validation.ConstraintViolationException
 class ErrorController {
 
     @ExceptionHandler(value = [ConstraintViolationException::class])
-    fun validationHandler(constraintViolationException: ConstraintViolationException): GeneralWrapper<Nothing?> {
-        return GeneralWrapper(
+    fun validationHandler(constraintViolationException: ConstraintViolationException): ResponseEntity<GeneralWrapper<Nothing?>> {
+        return ResponseEntity(GeneralWrapper(
             code = 400,
             status = "BAD REQUEST",
             data = null,
             message = constraintViolationException.message
-        )
+        ), HttpStatus.BAD_REQUEST)
     }
 
 }
--- src/main/kotlin/hightech/toontust/core/controller/ProductController.kt
+++ src/main/kotlin/hightech/toontust/core/controller/ProductController.kt
@@ -4,11 +4,15 @@ import hightech.toontust.core.dto.request.CreateProductRequestDTO
 import hightech.toontust.core.dto.response.ProductResponseDTO
 import hightech.toontust.core.dto.response.base.GeneralWrapper
 import hightech.toontust.core.service.IProductService
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
-@RestController("/api")
+@RestController
+@RequestMapping(value = ["/api/v1"])
 class ProductController(private val productService: IProductService) {
 
     @PostMapping(
@@ -16,14 +20,14 @@ class ProductController(private val productService: IProductService) {
         produces = ["application/json"],
         consumes = ["application/json"]
     )
-    fun createProduct(@RequestBody body: CreateProductRequestDTO): GeneralWrapper<ProductResponseDTO> {
+    fun createProduct(@RequestBody body: CreateProductRequestDTO): ResponseEntity<GeneralWrapper<ProductResponseDTO>> {
         val productResponse = productService.create(body)
-        return GeneralWrapper(
+        return ResponseEntity(GeneralWrapper(
             code = 200,
             status = "OK",
             data = productResponse,
             message = null
-        )
+        ), HttpStatus.CREATED)
     }
 
 }
